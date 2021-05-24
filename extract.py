68d566799bcb8b0d6fa104e78dcc970bc109372f
Bob-Z <carabobz@gmail.com>
2021-05-24 22:13:06 +0200

    Generate title_text and title_bg options

--- extract.py
+++ extract.py
@@ -25,7 +25,7 @@ def get_description_generic(section):
     # Remove any number of space at beginning of line
     d = re.sub("^ *", "", section, flags=re.M)
 
-    # replace all new line not preceeded by ']' by a space
+    # replace all new line not preceded by ']' by a space
     dd = re.sub(r'(?<!\])\n', ' ', d)
 
     # Every line should be "complete name [xxxx]"
@@ -37,26 +37,26 @@ def get_description_generic(section):
 
     desc_list = re.split(r'\n', d)
     # split with ", " ignoring ',' placed in parenthesis
-    #desc_list = re.split(r', (?!(?:[^(]*\([^)]*\))*[^()]*\))', d)
+    # desc_list = re.split(r', (?!(?:[^(]*\([^)]*\))*[^()]*\))', d)
 
     escaped_list = []
     for d in desc_list:
         # escape for regex
         regex_escaped = re.escape(d)
-        # escpae for bach
+        # escpae for bash
         e = regex_escaped.replace('!', '\!')
         escaped_list.append(e.replace('"', '\\\"'))
 
     print("\n\nFound", str(len(desc_list)), "entries")
     return escaped_list, desc_list
 
+
 def get_description_softlist(section):
     # remove space at the beginning of line
     ddd = re.sub("^  ", "", section, flags=re.M)
 
     e = ddd.replace(']\n', '], ').replace(',\n', ', ').replace('\n', '').replace(',,', ',')
-        #.replace('  ','').replace('   ', ' ')
-
+    # .replace('  ','').replace('   ', ' ')
 
     # remove [redump.org]
     dd = re.sub(' \[[^\]]+\]', '', e)
@@ -73,7 +73,7 @@ def get_description_softlist(section):
     for d in desc_list:
         # escape for regex
         regex_escaped = re.escape(d)
-        # escpae for bach
+        # escape for bash
         e = regex_escaped.replace('!', '\!')
         escaped_list.append(e.replace('"', '\\\"'))
 
@@ -81,22 +81,22 @@ def get_description_softlist(section):
     return escaped_list, desc_list
 
 
-def print_generic_command(escaped_list):
+def print_generic_command(title, escaped_list):
     print(
-        "./randomame.py --description=\"" + ":::".join(
+        "./randomame.py --title_text=\"     MAME  " + version + "     :::" + title + "\" --title_bg=\"/media/4To/emu/mame/mame.png\" --description=\"" + ":::".join(
             escaped_list) + "\" --all --allow_preliminary --timeout=60000 --window=1 --linear --quit --loose_search /media/4To/emu/mame/mame/mame")
 
 
-def print_softlist_command(escaped_list):
+def print_softlist_command(title, softlist_name, escaped_list):
     print(
-        "./randomame.py --selected_softlist=" + softlist_name + " --allow_not_supported --description=\"" + ":::".join(
+        "./randomame.py --title_text=\"     MAME  " + version + "     :::" + title + ":::" + softlist_name + "\" --title_bg=\"/media/4To/emu/mame/mame.png\" --selected_softlist=" + softlist_name + " --allow_not_supported --description=\"" + ":::".join(
             escaped_list) + "\" --timeout=60000 --window=1 --linear --quit /media/4To/emu/mame/mame/mame")
 
 
 def print_music(escaped_list, desc_list):
     index = 0
     for e in escaped_list:
-        filename = desc_list[index].replace('/','\\')
+        filename = desc_list[index].replace('/', '\\')
         print(
             "./randomame.py --music --allow_not_supported --description=\"" + e +
             "\" --timeout=120 --window=1 --linear --quit --start_command=\"./start_script.sh\" --end_command=\"./end_script.sh \'" + filename + "\'\" /media/4To/emu/mame/mame/mame\n")
@@ -110,18 +110,24 @@ generic_section = ["\nNew working machines\n--------------------\n", "\nNew work
 p = get_whats_new_page(sys.argv[1]).decode("utf-8")
 page = unicodedata.normalize("NFKD", p)
 
+version = page[2] + page[3] + page[4]
+
 for section in generic_section:
     section_page = get_section(page, section)
     if section_page is not None:
         escaped_list, desc_list = get_description_generic(section_page)
         print(section)
-        print_generic_command(escaped_list)
+        title = re.split("\n", section)
+        print_generic_command(title[1], escaped_list)
 
 softlist_section = ["\nNew working software list additions\n-----------------------------------",
                     "\nSoftware list items promoted to working\n---------------------------------------"]
 
 for section in softlist_section:
     print(section)
+
+    title_split = re.split("\n", section)
+    title = title_split[1]
 
     section_page = get_section(page, section)
     if section_page is not None:
@@ -140,4 +146,4 @@ for section in softlist_section:
             if softlist_name == "vgmplay":
                 print_music(escaped_list, desc_list)
             else:
-                print_softlist_command(escaped_list)
+                print_softlist_command(title, softlist_name, escaped_list)
